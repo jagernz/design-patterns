12b594092002dff1fbf5778b513f181d6bed169b
Your Name <you@example.com>
2017-10-19 23:09:02 +0300

    test_AbstractFactory

--- public/index.php
+++ public/index.php
@@ -2,3 +2,9 @@
 
 require_once '../DesignPatterns/start.php';
 
+$text = new DesignPatterns\Creational\AbstractFactory\HtmlFactory();
+$new  = $text->createText('хуйня');
+
+echo '<pre>';
+var_dump($new);
+echo '</pre>';
